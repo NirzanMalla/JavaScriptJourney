bb97cc4e61ae2718d5d102f0f8ee85a4960b01c0
Nirajan Bikram Malla Thakuri <125073944+NirzanMalla@users.noreply.github.com>
2023-06-01 11:29:18 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("Hello World")
+console.log("Name", "Nirajan");    //eeuta label hunxa eeuta data hunxa
+let a = "Ram";
+let b = 5;
+console.log(`the name of the client is ${a}, and his roll no is ${b}`);
+
+const num1 = 8;
+console.log(num1)
+
+const num2 = 5;
+console.log(num1 + num2);
+
+// num1 = 9; const lai update garna mildaina 
+b = 100; //change gara milxa let mah chai 
+// console.log(b)
+var d = 25;
+// console.log(d)
+d = 20;
+console.log(d)
+{
+    let b = 250;
+    console.log(b) //block vitra xuttai decalare garna painxa
+}
+console.log(b)
