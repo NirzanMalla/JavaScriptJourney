c6c85ae5c648a84647f73e3fff453973e54f90c2
Nirajan Bikram Malla Thakuri <125073944+NirzanMalla@users.noreply.github.com>
2023-06-02 18:15:37 +0545

    Add files via upload
    datatypes in js

---
+++ Datatypes.js
@@ -0,0 +1,45 @@
+// String
+// Number
+// Boolean
+// null
+// undefined 
+// Array
+// Object
+let var1 = "Hello";
+console.log(typeof var1);
+
+let var2 = `Hello
+world`  //line break grauna use hunxa
+console.log(var2)
+
+let name = "Nirajan"
+let var3 = "Good Afternoon " +name +", How was your Day?"
+console.log(var3)
+
+let var4 = `Good Afternoon ${name}, How was your day?` //${name} yoh cahi backtick vitra matra milxa
+// let var5 = 'their father name is hari'
+// console.log(var5);
+var1 = var1.toUpperCase();
+console.log(var1);
+
+console.log(var3.startsWith("good"));
+
+//Number
+
+let num1 = 45;
+console.log(typeof num1);
+
+//boolean
+let bol1 = true;
+console.log(typeof bol1);
+
+//null
+let nul1 = null;
+console.log(typeof nul1);
+//undefined
+let nul2
+console.log(typeof nul2);
+//empty string
+let nul3 = "";
+console.log(typeof nul3);
+
